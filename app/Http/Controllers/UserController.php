7974314b167417e5ff24dec1f058bc8662fe224c
Angga Cakraswara <cakraswara.all.gaming@example.com>
2026-06-14 17:54:37 +0700

    Mengajukan pull request untuk update data Terbaru UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Hash;
 
 use App\Models\User;
 use App\Http\Resources\UserResource;
-
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 class UserController extends Controller
 {
     /**
@@ -30,7 +30,8 @@ class UserController extends Controller
     //Esa Faiz Nurdin
     //231011701143
     public function store(Request $request)
-    {
+{
+    try {
         $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|email|unique:users,email',
@@ -44,7 +45,14 @@ class UserController extends Controller
         ]);
 
         return new UserResource($user);
+
+    } catch (\Exception $e) {
+        return response()->json([
+            'status' => 'error',
+            'message' => 'Terjadi kesalahan'
+        ], 500);
     }
+}
     /**
      * Display the specified resource.
      *
@@ -56,27 +64,56 @@ class UserController extends Controller
     //231011701331
     public function show($id)
     {
-         $user = User::findOrFail($id);
+        try {
+            $user = User::findOrFail($id);
 
-        return new UserResource($user);
+            return new UserResource($user);
+        } catch (ModelNotFoundException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'User tidak ditemukan'
+            ], 404);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Terjadi kesalahan pada server',
+                'error' => $e->getMessage()
+            ], 500);
+        }
     }
 
-    
     /**
      * Update the specified resource in storage.
-     * updated by : Angga Cakraswara - 231011700018
+     *  Angga Cakraswara - 231011700018
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
     {
-        
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:255|unique:users,email,' . $id,
+            'password' => 'nullable|string|min:6',
+        ]);
+
+        $user = User::findOrFail($id);
+
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+
+        if ($request->filled('password')) {
+            $user->password = Hash::make($request->input('password'));
+        }
+
+        $user->save();
+
+        return new UserResource($user);
     }
 
     /**
      * Remove the specified resource from storage.
-     *
+     * Rizki Putra
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
