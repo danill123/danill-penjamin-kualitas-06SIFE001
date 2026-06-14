c812bd3de1eef7be93c876f48dddc62b8f10dc37
Angga Cakraswara <cakraswara.all.gaming@example.com>
2026-06-14 11:38:58 +0700

    Menyelesaikan edit kode untuk update data pada UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 use App\Models\User;
 use App\Http\Resources\UserResource;
@@ -42,16 +43,34 @@ class UserController extends Controller
         //
     }
 
+    
     /**
      * Update the specified resource in storage.
-     *
+     * updated by : Angga Cakraswara - 231011700018
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:255|unique:users,email,' . $id,
+            'password' => 'nullable|string|min:6',
+        ]);
+
+        $user = User::findOrFail($id);
+
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+
+        if ($request->filled('password')) {
+            $user->password = Hash::make($request->input('password'));
+        }
+
+        $user->save();
+
+        return new UserResource($user);
     }
 
     /**
